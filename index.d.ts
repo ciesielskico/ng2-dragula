0112ee56647cf8bf596a215da74aba84cd831e36
Alexander Ciesielski <alexander@ciesielski.co>
2017-09-08 16:17:51 +0200

    wip

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+export { dragula } from './dist/components/dragula.class';
+export { DragulaDirective } from './dist/components/dragula.directive';
+export { DragulaService } from './dist/components/dragula.provider';
+export { DragulaModule } from './dist/components/dragular.module';
---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+var dragula_class_1 = require("./dist/components/dragula.class");
+exports.dragula = dragula_class_1.dragula;
+var dragula_directive_1 = require("./dist/components/dragula.directive");
+exports.DragulaDirective = dragula_directive_1.DragulaDirective;
+var dragula_provider_1 = require("./dist/components/dragula.provider");
+exports.DragulaService = dragula_provider_1.DragulaService;
+var dragular_module_1 = require("./dist/components/dragular.module");
+exports.DragulaModule = dragular_module_1.DragulaModule;
